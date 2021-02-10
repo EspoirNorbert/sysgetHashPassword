d5f7224a72977d54feee663ebacbf1fc4b180b78
EspoirNorbert <espoirnorbert@gmail.com>
2021-02-10 23:11:10 +0100

    Creation de la classe Addition qui additonne deux nombres entiers

---
+++ hachage-mdp/src/com/sysget/hachage/Addition.java
@@ -0,0 +1,13 @@
+package com.sysget.hachage;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int resultat, a , b;
+		a=3;
+		b=4;
+		resultat=a+b;
+		System.out.println("l'addition entre " + a + " et " + b+ " font : "  + resultat );
+	}
+
+}
